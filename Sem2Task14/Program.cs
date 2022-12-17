d66375e940dcbfaad2247b286f653c10aa684bf9
Mary <marysok-16@mail.ru>
2022-12-17 14:51:51 +0600

    add solutin for Task 9,11,12,14,16

---
+++ Sem2Task14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №14
+// Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+
+int number = int.Parse(Console.ReadLine()??"0");
+
+int result01 = number%7;
+int result02 = number%23;
+
+if ((result01==0)&&(result02==0))
+{
+    Console.WriteLine("Число " + number + " ДЕЛИТСЯ на число 7 и 23 без остатка");
+}
+else
+{
+    Console.WriteLine("Число " + number + " НЕ делится на число 7 и 23 без остатка");
+}
---
+++ Sem2Task9/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача №9
+// Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и
+// показывает наибольшую цифру числа.
+
+// Создаем Рандомайзер
+System.Random numberSintezator = new System.Random();
+
+// Получаем рандомное число
+int number = numberSintezator.Next(10,100);
+
+// выводим данные в консоль
+Console.WriteLine(number);
+
+//Получаем первое и второе число по отдельности
+
+int firstNumber = number/10; //  72:10 = 7,2 (убирает ",2", остается 7) !При условии, что в начале int
+int secondNumber = number%10; // 72:10 = 7,2 (убирает "7,", остается 2)
+
+// Сравниваем числа
+if (firstNumber > secondNumber)
+{
+    // Выводим данные в консоль
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    // Выводим данные в консоль
+    Console.WriteLine(secondNumber);
+}
+
+// Вариант 2
+
+// char[] digits = numberSintezator.Next(10,100).ToString().ToCharArray();
+// Console.WriteLine(digits);
+
+// firstNumber = ((int)digits[0])-48;
+// secondNumber = ((int)digits[1])-48;
+
+// int resultNumber = firstNumber > secondNumber?resultNumber = firstNumber:resultNumber = secondNumber;
+// Console.WriteLine(resultNumber);
